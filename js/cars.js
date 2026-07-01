22da9daf71202c1a7d37e75ca59692c50fa844e1
Vektor-S2 <vektor.sergej@yandex.ru>
2026-07-01 20:53:26 +0300

    Update ACC analytics data

--- js/cars.js
+++ js/cars.js
@@ -1,44 +1,77 @@
-const ACC_CARS = {
+console.log("ACC car names loaded v8");
 
-    1: "Porsche 991 GT3 R",
-    2: "Mercedes AMG GT3",
-    3: "Ferrari 488 GT3",
-    4: "Audi R8 LMS",
-    5: "Lamborghini Huracan GT3",
-    6: "McLaren 650S GT3",
-    7: "Nissan GT-R Nismo GT3",
-    8: "BMW M6 GT3",
-    9: "Bentley Continental GT3 2018",
-    10: "Porsche 991 II GT3 Cup",
-    11: "Nissan GT-R Nismo GT3 2018",
-    12: "Bentley Continental GT3 2016",
-    13: "Aston Martin V12 Vantage GT3",
-    14: "Lamborghini Gallardo R-EX",
-    15: "Jaguar G3",
-    16: "Lexus RC F GT3",
-    17: "Lamborghini Huracan ST",
-    18: "Audi R8 LMS Evo",
-    19: "AMR V8 Vantage GT3",
-    20: "Honda NSX GT3",
-    21: "Lamborghini Huracan Evo GT3",
-    22: "McLaren 720S GT3",
-    23: "Porsche 911 II GT3 R",
-    24: "Ferrari 488 GT3 Evo",
-    25: "Mercedes AMG GT3 Evo",
-    26: "Honda NSX GT3 Evo",
-    27: "McLaren 720S GT3 Evo",
-    28: "Porsche 992 GT3 Cup",
-    29: "BMW M4 GT3",
-    30: "Audi R8 LMS Evo II",
-    31: "Ferrari 296 GT3",
-    32: "Porsche 992 GT3 R",
+window.ACC_CARS = {
+    0: "Porsche 991 GT3 R",
+    1: "Mercedes-AMG GT3",
+    2: "Ferrari 488 GT3",
+    3: "Audi R8 LMS",
+    4: "Lamborghini Huracan GT3",
+    5: "McLaren 650S GT3",
+    6: "Nissan GT-R Nismo GT3 2018",
+    7: "BMW M6 GT3",
+    8: "Bentley Continental GT3 2018",
+    9: "Porsche 991 II GT3 Cup",
+    10: "Nissan GT-R Nismo GT3 2017",
+    11: "Bentley Continental GT3 2016",
+    12: "Aston Martin V12 Vantage GT3",
+    13: "Lamborghini Gallardo R-EX",
+    14: "Jaguar G3",
+    15: "Lexus RC F GT3",
+    16: "Lamborghini Huracan Evo (2019)",
+    17: "Honda NSX GT3",
+    18: "Lamborghini Huracan SuperTrofeo",
+    19: "Audi R8 LMS Evo (2019)",
+    20: "AMR V8 Vantage (2019)",
+    21: "Honda NSX Evo (2019)",
+    22: "McLaren 720S GT3 (2019)",
+    23: "Porsche 911 II GT3 R (2019)",
+    24: "Ferrari 488 GT3 Evo 2020",
+    25: "Mercedes-AMG GT3 2020",
+    26: "Ferrari 488 Challenge Evo",
+    27: "BMW M2 CS Racing",
+    28: "Porsche 911 GT3 Cup (Type 992)",
+    29: "Lamborghini Huracan Super Trofeo EVO2",
+    30: "BMW M4 GT3",
+    31: "Audi R8 LMS GT3 Evo II",
+    32: "Ferrari 296 GT3",
     33: "Lamborghini Huracan Evo2",
-    34: "Mercedes AMG GT3 Evo 2022",
-    35: "BMW M4 GT3 2022",
-    36: "Ford Mustang GT3"
+    34: "Porsche 992 GT3 R",
+    35: "McLaren 720S GT3 Evo 2023",
+    36: "Ford Mustang GT3",
+    50: "Alpine A110 GT4",
+    51: "AMR V8 Vantage GT4",
+    52: "Audi R8 LMS GT4",
+    53: "BMW M4 GT4",
+    55: "Chevrolet Camaro GT4",
+    56: "Ginetta G55 GT4",
+    57: "KTM X-Bow GT4",
+    58: "Maserati MC GT4",
+    59: "McLaren 570S GT4",
+    60: "Mercedes-AMG GT4",
+    61: "Porsche 718 Cayman GT4",
+    80: "Audi R8 LMS GT2",
+    82: "KTM X-BOW GT2",
+    83: "Maserati MC20 GT2",
+    84: "Mercedes-AMG GT2",
+    85: "Porsche 911 GT2 RS CS Evo",
+    86: "Porsche 935"
 };
 
-function getCarName(carModel)
+window.getCarName = function(carModel)
 {
-    return ACC_CARS[carModel] || ("Model " + carModel);
-}
+    if (carModel === null || carModel === undefined || carModel === "") {
+        return "-";
+    }
+
+    const numericModel = Number(carModel);
+
+    if (Number.isFinite(numericModel) && window.ACC_CARS[numericModel]) {
+        return window.ACC_CARS[numericModel];
+    }
+
+    if (window.ACC_CARS[carModel]) {
+        return window.ACC_CARS[carModel];
+    }
+
+    return "Model " + carModel;
+};
